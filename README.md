d154de63ee9c120084a40acb3e84327422f20f9f
Haniel Ubogu <hanieltobi@gmail.com>
2023-02-20 12:54:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a cli tool that replaces the extension of files in a directory and all subdirectories with a given extension, and also replaces the occurrence of a string in a file with a new one
+
+## Usage
+
+```bash
+$ replace --dir path/to/dir oldext newext --file path/to/file oldstr newstr
+```
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn replace_extension(dir: &str, old_ext: &str, new_ext: &str) {
+    let path = Path::new(dir);
+    if path.is_dir() {
+        for entry in fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let entry_path = entry.path();
+            if entry_path.is_dir() {
+                replace_extension(
+                    entry_path.to_str().unwrap(),
+                    old_ext,
+                    new_ext,
+                );
+            } else {
+                let file_name =
+                    entry_path.file_name().unwrap().to_str().unwrap();
+                if file_name.ends_with(old_ext) {
+                    let new_file_name = file_name.replace(old_ext, new_ext);
+                    let new_path = entry_path.with_file_name(new_file_name);
+                    fs::rename(entry_path, new_path).unwrap();
+                }
+            }
+        }
+    }
+}
+
+fn replace_string_in_file(file_path: &str, old_string: &str, new_string: &str) {
+    let mut file = fs::OpenOptions::new().write(true).open(file_path).unwrap();
+    let file_content = fs::read_to_string(file_path).unwrap();
+    let new_content = file_content.replace(old_string, new_string);
+    file.set_len(0).unwrap();
+    std::io::Write::write_all(&mut file, new_content.as_bytes()).unwrap();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} --dir <directory> <old_extension> <new_extension> --file <file_path> <old_string> <new_string>", args[0]);
+        return;
+    }
+
+    let mut dir_flag = false;
+    let mut file_flag = false;
+    let mut dir = "";
+    let mut old_ext = "";
+    let mut new_ext = "";
+    let mut file_path = "";
+    let mut old_string = "";
+    let mut new_string = "";
+
+    for i in 1..args.len() {
+        if args[i] == "--dir" {
+            dir_flag = true;
+            dir = &args[i + 1];
+            old_ext = &args[i + 2];
+            new_ext = &args[i + 3];
+        } else if args[i] == "--file" {
+            file_flag = true;
+            file_path = &args[i + 1];
+            old_string = &args[i + 2];
+            new_string = &args[i + 3];
+        }
+    }
+
+    if dir_flag {
+        replace_extension(dir, old_ext, new_ext);
+    }
+    if file_flag {
+        replace_string_in_file(file_path, old_string, new_string);
+    }
+}
